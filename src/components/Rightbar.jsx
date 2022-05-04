785eb8828406d781a71691466eef3adbaec904e1
Garrett <glawless@eagles.ewu.edu>
2022-05-04 00:22:17 -0700

    🚧

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
-import { Box } from '@mui/material'
-
+import React from "react";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
 
 const Rightbar = () => {
   return (
-    <Box bgcolor="pink" flex={2} padding={2}>Rightbar</Box>
-  )
-}
+    <Box flex={2} padding={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup spacing="small">
+          <Avatar sx={{ bgcolor: "azure" }} alt="Rex" src="/broken-image.jpg" />
+          <Avatar
+            sx={{ bgcolor: "deepskyblue" }}
+            alt="Troy"
+            src="/broken-image.jpg"
+          />
+          <Avatar
+            sx={{ bgcolor: "darkorange" }}
+            alt="Dozer"
+            src="/broken-image.jpg"
+          />
+          <Avatar
+            sx={{ bgcolor: "azure" }}
+            alt="Hank"
+            src="/broken-image.jpg"
+          />
+          <Avatar
+            sx={{ bgcolor: "deepskyblue" }}
+            alt="Pickle"
+            src="/broken-image.jpg"
+          />
+        </AvatarGroup>
+        <Typography
+          variant="h6"
+          fontWeight={100}
+          marginTop={2}
+          marginBottom={2}
+        >
+          Recent Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/breakfast.jpg"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/burgers.jpg"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/bike.jpg"
+              alt=""
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography
+          variant="h6"
+          fontWeight={100}
+          marginTop={2}
+          marginBottom={2}
+        >
+          News Around You
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
-export default Rightbar
+export default Rightbar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,72 +22,74 @@ import React from "react";
 const Sidebar = () => {
   return (
     <Box flex={1} padding={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#home">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Homepage" />
-          </ListItemButton>
-        </ListItem>
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+        </List>
         <ListItem disablePadding>
           <ListItemButton component="a" href="#simple-list">
             <ListItemIcon>
-              <Article />
+              <ModeNight />
             </ListItemIcon>
-            <ListItemText primary="Pages" />
+            <Switch />
           </ListItemButton>
         </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list">
-            <ListItemIcon>
-              <Group />
-            </ListItemIcon>
-            <ListItemText primary="Groups" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list">
-            <ListItemIcon>
-              <Storefront />
-            </ListItemIcon>
-            <ListItemText primary="Marketplace" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list">
-            <ListItemIcon>
-              <Person />
-            </ListItemIcon>
-            <ListItemText primary="Friends" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list">
-            <ListItemIcon>
-              <Settings />
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list">
-            <ListItemIcon>
-              <AccountBox />
-            </ListItemIcon>
-            <ListItemText primary="Profile" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <ListItem disablePadding>
-        <ListItemButton component="a" href="#simple-list">
-          <ListItemIcon>
-            <ModeNight />
-          </ListItemIcon>
-          <Switch />
-        </ListItemButton>
-      </ListItem>
+      </Box>
     </Box>
   );
 };
